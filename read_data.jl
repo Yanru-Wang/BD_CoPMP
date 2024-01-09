bc0523da98a1a8b42a2cd49fb72242386958b3b0
wangyanru <627785608@qq.com>
2024-01-09 13:12:12 +0800

    save data

--- read_data.jl
+++ read_data.jl
@@ -49,20 +49,41 @@ end
 
 param = Param(0, "", "", "", 0)
 
+saveFname = ""
+
 for i in eachindex(ARGS)
     str = split(ARGS[i], "=")
     if str[1] == "testset"
         param.testset = parse(Int, str[2])
     elseif str[1] == "fname_demand"
         param.fname_demand = str[2]
+        m = match(r"(T1_H_[0-9]+_[0-9].copmp)", str[2])
+        println(str[2])
+        name = split(m.match, "_")
+        global saveFname = name[1] * "_" * name[3] * "_" * name[4]
     elseif str[1] == "fname_distance"
         param.fname_distance = str[2]
     elseif str[1] == "fname_real"
         param.fname_real = str[2]
+        global saveFname = "T" * string(param.testset) * "_"
     elseif str[1] == "size"
         param.size = parse(Int, str[2])
+        global saveFname *= str[2] * ".copmp"
     end
 end
 
 vec_h, mat_d = GetData(param)
+
+open(saveFname, "w") do f
+    for h in vec_h
+        write(f, "$h ")
+    end
+    write(f, "\n")
+    for i in 1:length(vec_h)
+        for d in mat_d[i, :]
+            write(f, "$d ")
+        end
+        write(f, "\n")
+    end
+end
 
